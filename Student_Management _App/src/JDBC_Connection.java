88d05d1db567068bfb86407076c8a2934e7066df
SachinVignesh330 <sachinvignesh097@gmail.com>
2025-09-14 14:57:34 +0530

    JDBC_Connection.java

--- Student_Management _App/src/JDBC_Connection.java
+++ Student_Management _App/src/JDBC_Connection.java
@@ -2,10 +2,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class JDBC_Connection {
-    // Method to establish and return a DB connection
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/student_console", "root", "12102000");
     }
 }
+
